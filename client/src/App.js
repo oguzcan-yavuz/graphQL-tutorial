ec0d57ad83721dc608b54304688fc21a3e992581
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2018-06-13 00:58:18 +0300

    components modified. BookList component added. ApolloClient implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import BookList from './components/BookList'
+import { SERVER_URI } from './config.json'
+
+const client = new ApolloClient({
+  uri: SERVER_URI
+})
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={client}>
+        <div id='main'>
+          <h1>Hello World!</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     )
   }
 }
