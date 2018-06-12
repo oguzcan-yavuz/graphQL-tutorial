07d644321fbe6c69c2f1a2c3e786850310e6336e
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2018-06-13 01:22:13 +0300

    AddBook component created and implemented

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 import BookList from './components/BookList'
+import AddBook from './components/AddBook'
 import { SERVER_URI } from './config.json'
 
 const client = new ApolloClient({
@@ -15,6 +16,7 @@ class App extends Component {
         <div id='main'>
           <h1>Hello World!</h1>
           <BookList />
+          <AddBook />
         </div>
       </ApolloProvider>
     )
