9a3722817b24678796a7b8a8240599a06e6598a1
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2018-06-12 19:31:01 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# GraphQL Tutorial
+
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
+
+### Notes:
+
+- `args` field in `GraphQLObjectType` determines the arguments when client
+asks for a schema.
+
+- 
---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const logger = require('morgan')
+const schema = require('./schema/schema.js');
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+  schema
+}))
+app.use(logger('dev'))
+
+app.listen(4000, () => {
+  console.log('Server is listening on port 4000')
+})
